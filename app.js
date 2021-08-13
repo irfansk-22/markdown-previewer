6c8445072c128f934aafd61c1a29e695be9e5f65
Irfan Shaikh <irfansk.dev@gmail.com>
2021-08-14 01:18:11 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,80 @@
+const defaultMarkup = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == \'\`\`\`\' && lastLine == \'\`\`\`\') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo](data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K)
+
+`;
+
+const App = () => {
+  const [text, setText] = React.useState(defaultMarkup);
+  const markdown = marked(text, {breaks: true});
+
+  return (
+    <div className='container'>
+      <h1 className='text-center m-4 mt-3 title'>Markdown Previewer</h1>
+      <div className='row'>
+        <div className='col-lg-6 col-12 mb-4'>
+          <h5 className="text-white">Enter markdown here: </h5>
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className='form-control'
+            id='editor'
+            cols='30'
+            rows='10'
+          />
+        </div>
+        <div className='col-lg-6 col-12 mb-4'>
+          <h5 className="text-white">See the result: </h5>
+          <div
+            className='preview rounded'
+            id='preview'
+            dangerouslySetInnerHTML={{ __html: markdown }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
